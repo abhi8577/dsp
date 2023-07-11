fae0f682c45b31b595aa552f6bb32360de7be5ad
abhi1019 <abhisheak1019@gmail.com>
2023-07-11 11:34:59 +0530

    DiscomUserController

---
+++ src/main/java/com/dsp/dsp/controller/DiscomUserController.java
@@ -0,0 +1,10 @@
+package com.dsp.dsp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/discom_user")
+public class DiscomUserController {
+
+}
